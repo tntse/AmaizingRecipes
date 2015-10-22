19f9eed523cb309643cc114b321896213fb41203
Tricia Nemiroff <tricianemiroff@sfs-wifi-dhcp-10-143-37-98.wifi.sfsu.edu>
2015-10-22 14:44:09 -0700

    edited .gitignore

--- AmaizingUnicornRecipes/app/src/main/java/com/taeyeona/amaizingunicornrecipes/MapsActivity.java
+++ AmaizingUnicornRecipes/app/src/main/java/com/taeyeona/amaizingunicornrecipes/MapsActivity.java
@@ -33,7 +33,7 @@ public class MapsActivity extends FragmentActivity {
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
         }
-        
+
     }
 
     private void buildAlertMessageNoGps() {
