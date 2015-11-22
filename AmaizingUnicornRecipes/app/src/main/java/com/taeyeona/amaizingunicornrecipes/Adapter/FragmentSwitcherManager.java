ddaa0c97200d52b26c1f3e2622decc6b14358ed8
tntse <samoht.est@gmail.com>
2015-11-21 17:03:33 -0800

    Fixing #49

---
+++ AmaizingUnicornRecipes/app/src/main/java/com/taeyeona/amaizingunicornrecipes/Adapter/FragmentSwitcherManager.java
@@ -0,0 +1,95 @@
+package com.taeyeona.amaizingunicornrecipes.Adapter;
+
+import android.graphics.Typeface;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+/**
+ * Created by thomastse on 11/20/15.
+ */
+public class FragmentSwitcherManager implements View.OnClickListener, ViewPager.OnPageChangeListener{
+    private ArrayList<Button> buttons;
+    private ArrayList<View> bars;
+    private ViewPager theViewPager;
+    private ActivePage active;
+
+    public FragmentSwitcherManager(ViewPager viewPager){
+        buttons = new ArrayList<Button>();
+        bars = new ArrayList<View>();
+        theViewPager = viewPager;
+        theViewPager.addOnPageChangeListener(this);
+    }
+
+    public void add(Button newButton, View newBar){
+        newButton.setOnClickListener(this);
+        buttons.add(newButton);
+        bars.add(newBar);
+        if(active == null){
+            active = new ActivePage(newButton, newBar);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int index = buttons.indexOf((Button)view);
+        active.getButton().setTypeface(null, Typeface.NORMAL);
+        active.getView().setVisibility(View.INVISIBLE);
+
+        active.setButton(buttons.get(index));
+        active.setView(bars.get(index));
+        theViewPager.setCurrentItem(index);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        active.getButton().setTypeface(null, Typeface.NORMAL);
+        active.getView().setVisibility(View.INVISIBLE);
+
+        active.setButton(buttons.get(position));
+        active.setView(bars.get(position));
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    class ActivePage {
+
+        private Button button;
+        private View view;
+
+        public ActivePage(Button button, View view){
+            setButton(button);
+            setView(view);
+        }
+
+        public Button getButton() {
+            return button;
+        }
+
+        public View getView() {
+            return view;
+        }
+
+        public void setButton(Button button) {
+            this.button = button;
+            button.setTypeface(null, Typeface.BOLD);
+        }
+
+        public void setView(View view) {
+            this.view = view;
+            view.setVisibility(View.VISIBLE);
+        }
+
+    }
+}
--- AmaizingUnicornRecipes/app/src/main/java/com/taeyeona/amaizingunicornrecipes/IngredientsManager.java
+++ AmaizingUnicornRecipes/app/src/main/java/com/taeyeona/amaizingunicornrecipes/IngredientsManager.java
@@ -184,7 +184,7 @@ public class IngredientsManager implements Set<String> {
         for(int i = 0; i < list.size(); i++)
             ret += list.get(i) + '\n';
 
-        return ret.substring(0, ret.length()-1).split("\n");
+        return ret.trim().split("\n");
     }
 
     /**
@@ -220,7 +220,7 @@ public class IngredientsManager implements Set<String> {
                 ret += (String)ingredientList[i] +"\n\t";
         }
         Log.d(IngredientsManager.class.getSimpleName(), "ret: " + ret);
-        return ret.substring(0, ret.length()-1).split("\t");
+        return ret.trim().split("\t");
     }
 
     /**
