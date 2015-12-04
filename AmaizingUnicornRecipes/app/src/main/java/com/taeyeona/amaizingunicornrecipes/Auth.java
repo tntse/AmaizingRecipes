0cb2bfc7ccf575343f66f6d9457d36431ca40bb3
tntse <samoht.est@gmail.com>
2015-12-03 17:10:20 -0800

    new f2f key

--- AmaizingUnicornRecipes/app/src/main/java/com/taeyeona/amaizingunicornrecipes/Auth.java
+++ AmaizingUnicornRecipes/app/src/main/java/com/taeyeona/amaizingunicornrecipes/Auth.java
@@ -7,8 +7,9 @@ public class Auth {
 
     //http://food2fork.com/api/get?key=50ad55b48d8dbd791d8b69af229adeca
     // key numero dos: f2f a5b90f9d11f5453c04fc97ec2789ab79
+    // thomas's f2f key: 87c3746156e104d5055e4cca1a6e9042
     public static final String DEVELOPER_KEY = "AIzaSyAgluXYn35S2cVNEhiT07qGwN6B2uz7kyk";
-    public static final String F2F_Key = "50ad55b48d8dbd791d8b69af229adeca";
+    public static final String F2F_Key = "87c3746156e104d5055e4cca1a6e9042";
     public static final String MAPS_KEY = "AIzaSyAdMLf1_CLLZr7BVP7Li0o4EorZCQE0S_I";
     public static final String GET_URL = "http://food2fork.com/api/get";
     public static final String URL = "http://food2fork.com/api/search";
